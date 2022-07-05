eebb4b8cf9b95e38e9d631712029907edc1997c8
kaicLimaOliveira <kaic10@outlook.com>
2022-07-05 19:49:23 -0300

    Segunda aula concluída

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,13 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
+import { Event } from "./pages/Event"
 
-interface Lesson {
-  id: string,
-  title: string
-}
+
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
-  console.log(data);
-
-
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    <div>
+      <Event />
+    </div>
   )
 }
 
